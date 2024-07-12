76bb6fe22fc005912fb96243705fcb9136ad8376
Max Oberaigner <contact@oberaigner.info>
2024-07-12 13:23:07 +0200

    MobileUI: Enable DistractionFreeMode per default (bugfix)

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -447,7 +447,13 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 	EDITOR_SETTING(Variant::STRING, PROPERTY_HINT_GLOBAL_FILE, "interface/editor/main_font", "", "*.ttf,*.otf,*.woff,*.woff2,*.pfb,*.pfm")
 	EDITOR_SETTING(Variant::STRING, PROPERTY_HINT_GLOBAL_FILE, "interface/editor/main_font_bold", "", "*.ttf,*.otf,*.woff,*.woff2,*.pfb,*.pfm")
 	EDITOR_SETTING(Variant::STRING, PROPERTY_HINT_GLOBAL_FILE, "interface/editor/code_font", "", "*.ttf,*.otf,*.woff,*.woff2,*.pfb,*.pfm")
-	_initial_set("interface/editor/separate_distraction_mode", false);
+
+#ifdef MOBILE_UI_ENABLED
+    _initial_set("interface/editor/separate_distraction_mode", true);
+#else
+    _initial_set("interface/editor/separate_distraction_mode", false);
+#endif
+
 	_initial_set("interface/editor/automatically_open_screenshots", true);
 	EDITOR_SETTING_USAGE(Variant::BOOL, PROPERTY_HINT_NONE, "interface/editor/single_window_mode", false, "", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_RESTART_IF_CHANGED)
 	_initial_set("interface/editor/mouse_extra_buttons_navigate_history", true);
